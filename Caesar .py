8027f861f5ecd812c74d149dc5b33e1f214f9f41
llakeTuk <107436938+llakeTuk@users.noreply.github.com>
2022-06-18 16:44:21 +0300

    Add files via upload
    Caesar method of encryption

---
+++ Caesar .py
@@ -0,0 +1,21 @@
+alphabet = ' ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+y = 'y'
+while y == 'y':
+  text = input("enter text for encryption ", )
+  offset = int(input("enter offset(key) ", ))
+  endtext = ''
+  action = input("Do you want to encrypt or decrypt?(en/de) ", )
+  if action == 'en':
+    for i in text:
+      placeintext = alphabet.find(i)
+      endplace = placeintext + offset
+      endtext += alphabet[endplace % 27]
+  elif action == 'de':
+    for i in text:
+      placeintext = alphabet.find(i)
+      endplace = placeintext - offset
+      endtext += alphabet[endplace % 27]
+  else:
+    print("enter en or de")
+  print(endtext)
+  y = input("Do you want to start new operation?(y/n) ")
